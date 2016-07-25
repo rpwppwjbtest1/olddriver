52583a692904223e3b00e757ee4ff44ac031f6e6
AbracadabraH <1072539508@qq.com>
2016-07-25 19:03:50 +0800

    v3

--- app/src/main/java/com/example/rp/rpwpphjb/MainActivity.java
+++ app/src/main/java/com/example/rp/rpwpphjb/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //v2
+        //v3
     }
 }
